e8c77f448a03d45cbd0d9d415c53483427b0a1e7
alexsanjoseph <alexsanjoseph@gmail.com>
2022-12-26 00:49:56 +0530

    Generic answers

---
+++ lib/answer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String answerText;
+  final VoidCallback selectHandler;
+
+  Answer(this.answerText, this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: selectHandler,
+        child: Text(answerText),
+        // style: ButtonStyle(
+        //   backgroundColor: MaterialStateProperty.all(Colors.red),
+        //   foregroundColor: MaterialStateProperty.all(Colors.black),
+        // ),
+      ),
+    );
+  }
+}
